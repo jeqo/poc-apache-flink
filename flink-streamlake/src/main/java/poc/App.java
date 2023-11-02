3aff8ed2d496d06253f8ecba27043d6c70fdf482
Jorge Esteban Quilcate Otoya <jeqo@pm.me>
2023-11-03 00:35:24 +0200

    feat: join started/finished events

--- flink-streamlake/src/main/java/poc/App.java
+++ flink-streamlake/src/main/java/poc/App.java
@@ -1,21 +1,18 @@
 package poc;
 
 import com.twitter.chill.java.UnmodifiableMapSerializer;
-import de.javakaffee.kryoserializers.UnmodifiableCollectionsSerializer;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
-import org.apache.flink.connector.kafka.sink.KafkaSink;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.connector.kafka.source.reader.deserializer.KafkaRecordDeserializationSchema;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.ProcessJoinFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
 import org.apache.kafka.server.log.remote.storage.RemoteLogMetadata;
 import org.apache.kafka.server.log.remote.storage.RemoteLogSegmentMetadata;
 import org.apache.kafka.server.log.remote.storage.RemoteLogSegmentMetadataUpdate;
+import org.apache.kafka.server.log.remote.storage.RemoteLogSegmentState;
 
 public class App {
     final StreamExecutionEnvironment env;
@@ -38,103 +35,51 @@ public class App {
         final var source = KafkaSource.<RemoteLogMetadata>builder()
             .setBootstrapServers("localhost:9092")
             .setTopics("__remote_log_metadata")
-            .setGroupId("flink-app-v1")
+            .setGroupId("flink-app-v3")
             .setStartingOffsets(OffsetsInitializer.earliest())
             .setDeserializer(deserializer)
             .build();
         final var kafkaSource = env.fromSource(source, WatermarkStrategy.noWatermarks(), "kafka:remote-log-metadata");
 
-        final var serializer = KafkaRecordSerializationSchema.builder()
-            .setValueSerializationSchema(new SimpleStringSchema())
-            .setTopic("output-t1")
-            .build();
+        final var started =
+            kafkaSource
+                .filter(remoteLogMetadata -> remoteLogMetadata instanceof RemoteLogSegmentMetadata)
+                .map(remoteLogMetadata -> (RemoteLogSegmentMetadata) remoteLogMetadata)
+                .filter(remoteLogSegmentMetadata -> remoteLogSegmentMetadata.state().equals(RemoteLogSegmentState.COPY_SEGMENT_STARTED))
+                .keyBy(RemoteLogSegmentMetadata::remoteLogSegmentId);
 
-        final var kafkaSink = KafkaSink.<String>builder()
-            .setBootstrapServers("localhost:9092")
-            .setRecordSerializer(serializer)
-            .build();
+        final var finished =
+            kafkaSource
+                .filter(remoteLogMetadata -> remoteLogMetadata instanceof RemoteLogSegmentMetadataUpdate)
+                .map(remoteLogMetadata -> (RemoteLogSegmentMetadataUpdate) remoteLogMetadata)
+                .filter(remoteLogSegmentMetadataUpdate -> remoteLogSegmentMetadataUpdate.state().equals(RemoteLogSegmentState.COPY_SEGMENT_FINISHED))
+                .keyBy(RemoteLogSegmentMetadataUpdate::remoteLogSegmentId);
 
-        kafkaSource
-            .map(remoteLogMetadata -> remoteLogMetadata.topicIdPartition().toString())
-            .sinkTo(kafkaSink);
+        final var merged =
+            started.intervalJoin(finished)
+                .between(Time.hours(-1), Time.hours(1))
+                .process(new RemoteLogMetadataMerger());
+
+        merged.print();
     }
 
+    static class RemoteLogMetadataMerger
+        extends ProcessJoinFunction<RemoteLogSegmentMetadata, RemoteLogSegmentMetadataUpdate, RemoteLogSegmentMetadata> {
+            @Override
+            public void processElement(RemoteLogSegmentMetadata left,
+                                       RemoteLogSegmentMetadataUpdate right,
+                                       ProcessJoinFunction<RemoteLogSegmentMetadata, RemoteLogSegmentMetadataUpdate, RemoteLogSegmentMetadata>.Context ctx,
+                                       Collector<RemoteLogSegmentMetadata> out) {
+                out.collect(left.createWithUpdates(right));
+            }
+    }
+
+    // Run with 
+    // --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED 
     public static void main(String[] args) throws Exception {
         final var env = StreamExecutionEnvironment.getExecutionEnvironment();
         final var app = new App(env);
         app.build();
         env.execute("test-kafka");
-
-//		// Split up the lines in pairs (2-tuples) containing: (word,1)
-//        DataStream<String> counts = text.flatMap(new Tokenizer())
-//		// Group by the tuple field "0" and sum up tuple field "1"
-//		.keyBy(value -> value.f0)
-//		.sum(1)
-//		.flatMap(new Reducer());
-//
-//		// Add the sink to so results
-//		// are written to the outputTopic
-//        counts.sinkTo(sink);
-
-        // Execute program
-//
-//		var dataStream = env
-//            .socketTextStream("localhost", 9999)
-//            .flatMap(new Splitter())
-//            .keyBy(value -> value.f0)
-//            .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
-//            .sum(1);
-//
-//        dataStream.print();
-//
-//        env.execute("Window WordCount");
-    }
-
-
-    /**
-     * Implements the string tokenizer that splits sentences into words as a user-defined
-     * FlatMapFunction. The function takes a line (String) and splits it into multiple pairs in the
-     * form of "(word,1)" ({@code Tuple2<String, Integer>}).
-     */
-    public static final class Tokenizer
-        implements FlatMapFunction<String, Tuple2<String, Integer>> {
-
-        @Override
-        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
-            // Normalize and split the line
-            String[] tokens = value.toLowerCase().split("\\W+");
-
-            // Emit the pairs
-            for (String token : tokens) {
-                if (token.length() > 0) {
-                    out.collect(new Tuple2<>(token, 1));
-                }
-            }
-        }
-    }
-
-
-    // Implements a simple reducer using FlatMap to
-    // reduce the Tuple2 into a single string for 
-    // writing to kafka topics
-    public static final class Reducer
-        implements FlatMapFunction<Tuple2<String, Integer>, String> {
-
-        @Override
-        public void flatMap(Tuple2<String, Integer> value, Collector<String> out) {
-            // Convert the pairs to a string
-            // for easy writing to Kafka Topic
-            String count = value.f0 + " " + value.f1;
-            out.collect(count);
-        }
-    }
-
-    static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
-        @Override
-        public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) {
-            for (String word : sentence.split(" ")) {
-                out.collect(new Tuple2<>(word, 1));
-            }
-        }
     }
 }
